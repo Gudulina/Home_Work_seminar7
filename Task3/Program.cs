0c329e3bbdf8fc66a4dd1f6e75a13ee47ca7fcf6
Elena <gudulina@mail.ru>
2022-07-31 13:10:55 +0700

    Add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+void Print(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void ArithmeticMean(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    double res;
+
+    for (int i = 0; i < column; i++)
+    {
+        res = 0;
+        for (int j = 0; j < row; j++) res += arr[j, i];
+        Console.Write($"{Math.Round(res / row, 1)}; ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] array = MassNums(row, column, 1, 6);
+Print(array);
+
+ArithmeticMean(array);
